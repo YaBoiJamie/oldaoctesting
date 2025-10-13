8f459c3fd4d2b6f2318fbcab3a685391d5958527
YaBoiJamie <jamiedkuiper@gmail.com>
2025-10-13 23:39:55 +0200

    quick fix (#3)
    * day 2 progress
    
    * oude dingen proberen
    
    * Day 3 part 1
    
    * lay out fix?
    
    * security fix
    
    * lay out fix
    
    * PEP8 style added to the code

--- 2024/Day3/day3-part1.py
+++ 2024/Day3/day3-part1.py
@@ -3,28 +3,24 @@ import re
 with open('input.txt', 'r') as file:
     inputfile = file.read()
 
-def mul(x,y):
+
+def mul(x, y):
     return x * y
+
+
 check = re.compile(r'mul\([0-9]{1,3},[0-9]{1,3}\)')
 
 matches = check.findall(inputfile)
 # print(matches)
 totaal = 0
 for match in matches:
-    #print(match)
+    # print(match)
     muller = match.strip('mul()')
     # print(muller)
     yes = muller.split(',')
     # print(yes)
-    antwoord = mul(int(yes[0]),int(yes[1]))
+    antwoord = mul(int(yes[0]), int(yes[1]))
     totaal += antwoord
 print(totaal)
 
 print(inputfile)
-
-
-
-
-
-
-    
