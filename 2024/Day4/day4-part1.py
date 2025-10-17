5fcb0bfa42c9dadf672363c4278a22c8c76e8d20
YaBoidesktop <jamiedkuiper@gmail.com>
2025-10-17 23:01:30 +0200

    mapping ftw

--- 2024/Day4/day4-part1.py
+++ 2024/Day4/day4-part1.py
@@ -1,9 +1,38 @@
 with open('inputtest.txt', 'r') as inputfile:
-    file = inputfile.read().replace('\n', '')
+    file = inputfile.read().splitlines()
+
 
 map = {}
 
-for i, char in enumerate(file):
-    print(i, char)
-    map[i] = char
-print(map)
+for y,line in enumerate(file):
+    print('regel', y, line)
+    for x,char in enumerate(line):
+        if x not in map:
+            map[x] = {}
+        map[x][y] = char
+        print('map nu', map)
+
+def getPos(x,y,map):
+    """Geef waarde op positie x,y
+
+    Args:
+        x (int): Xpos (character)
+        y (int): Ypos (regel)
+        map (dict): de x/y map
+
+    Returns:
+        str: Waarde op x,y
+    """
+    if x not in map:
+        return False
+    if y not in map[x]:
+        return False
+    r = map[x][y]
+    return r
+
+
+
+
+
+print(getPos(4,0,map))
+
